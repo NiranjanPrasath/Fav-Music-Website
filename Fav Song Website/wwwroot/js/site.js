48652b490af5bff3c63420872d4eac2b1fb0cfbb
NiranjanPrasath <niranjan.prasathv@gmail.com>
2026-06-16 10:00:11 -0400

    Initial commit

---
+++ Fav Song Website/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+
+    const bars = document.querySelectorAll(".bar");
+
+    setInterval(() => {
+
+        bars.forEach(bar => {
+
+            const randomHeight =
+                Math.floor(Math.random() * 120) + 40;
+
+            bar.style.height =
+                randomHeight + "px";
+
+        });
